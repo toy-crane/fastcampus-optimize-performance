1bb6001011372157762d792745dffc6e4f511a7a
toycrane <alwaysfun2183@gmail.com>
2024-07-19 15:24:01 +0900

    feat: Shadcn 설치하기

--- apps/compare-phones/app/page.tsx
+++ apps/compare-phones/app/page.tsx
@@ -1,5 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 function Page() {
-  return <div className="text-3xl font-bold">Welcome to Compare Phones</div>;
+  return (
+    <div className="text-3xl font-bold flex-col items-center">
+      <h1>Welcome to Compare Phones</h1>
+      <Button className="w-full">Welcome</Button>
+    </div>
+  );
 }
 
 export default Page;
