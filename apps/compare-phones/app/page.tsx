ac117ac80b09f0600de8f2ab2a883236c4e76080
toycrane <alwaysfun2183@gmail.com>
2024-07-22 10:37:50 +0900

    feat: supabase client 생성하기

--- apps/compare-phones/app/page.tsx
+++ apps/compare-phones/app/page.tsx
@@ -1,6 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { createClient } from "@/utils/supabase/server";
 
-function Page() {
+async function Page() {
+  const supabase = createClient();
+  const { data, error } = await supabase
+    .from("phones")
+    .select("*, phone_colors(*)");
+
   return (
     <div className="text-3xl font-bold flex-col items-center">
       <h1>Welcome to Compare Phones</h1>
