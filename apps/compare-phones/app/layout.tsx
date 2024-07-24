8b8d0131abfb231c049b4713aa308170a0c3d1f6
toycrane <alwaysfun2183@gmail.com>
2024-07-24 15:10:57 +0900

    feat: header, main, footer layout 정의하기

--- apps/compare-phones/app/layout.tsx
+++ apps/compare-phones/app/layout.tsx
@@ -1,4 +1,6 @@
+import { Apple } from "lucide-react";
 import "./globals.css";
+import Link from "next/link";
 
 export default function RootLayout({
   children,
@@ -7,10 +9,25 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
-      <body>
-        <main className="min-h-screen flex flex-col items-center">
-          {children}
-        </main>
+      <body className="min-h-screen flex flex-col items-center">
+        <header className="container flex py-4">
+          <div className="flex">
+            <Apple className="w-8 h-8 mr-4" />
+            <h1 className="text-3xl font-bold">아이폰 비교하기</h1>
+          </div>
+        </header>
+        <main className="flex-1 border-b border-t container">{children}</main>
+        <footer className="container flex py-12">
+          <p className="text-sm text-muted-foreground">
+            Built by{" "}
+            <Link
+              href="https://github.com/toycrane"
+              className="underline underline-offset-4"
+            >
+              toy-crane
+            </Link>
+          </p>
+        </footer>
       </body>
     </html>
   );
--- apps/compare-phones/app/page.tsx
+++ apps/compare-phones/app/page.tsx
@@ -1,18 +1,5 @@
-import { Button } from "@/components/ui/button";
-import { createClient } from "@/utils/supabase/server";
-
 async function Page() {
-  const supabase = createClient();
-  const { data, error } = await supabase
-    .from("phones")
-    .select("*, phone_colors(*)");
-
-  return (
-    <div className="text-3xl font-bold flex-col items-center">
-      <h1>Welcome to Compare Phones</h1>
-      <Button className="w-full">Welcome</Button>
-    </div>
-  );
+  return <div className="text-2xl font-bold">Hello</div>;
 }
 
 export default Page;
