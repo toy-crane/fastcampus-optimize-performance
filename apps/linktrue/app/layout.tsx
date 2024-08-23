04053f684c6a438ce3b2305f1c50aed16cf86eb7
toycrane <alwaysfun2183@gmail.com>
2024-08-23 14:59:43 +0900

    프로젝트 초기화

---
+++ apps/linktrue/app/layout.tsx
@@ -0,0 +1,15 @@
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ko">
+      <body>
+        <main className="min-h-screen flex flex-col">{children}</main>
+      </body>
+    </html>
+  );
+}
