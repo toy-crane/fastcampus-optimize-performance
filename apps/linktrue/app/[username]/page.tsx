bc93e4d3aa879c5cb403b676c1d9e2b84f7cfd88
toycrane <alwaysfun2183@gmail.com>
2024-08-28 17:40:38 +0900

    CH06_02. 아바타 섹션 구현하기

--- apps/linktrue/app/[username]/page.tsx
+++ apps/linktrue/app/[username]/page.tsx
@@ -1,3 +1,4 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { createClient } from "@/utils/supabase/server";
 
 const Page = async ({ params }: { params: { username: string } }) => {
@@ -22,13 +23,21 @@ const Page = async ({ params }: { params: { username: string } }) => {
   }
 
   return (
-    <main className="flex min-h-screen flex-col p-24 mx-auto w-full md:w-[600px]">
-      {links?.map((link) => (
-        <div key={link.id}>
-          <div>{link.url}</div>
-          <div>{link.title}</div>
+    <main className="flex min-h-screen flex-col p-24 items-center">
+      <div className="w-full max-w-md">
+        <div className="flex flex-col items-center pt-10 pb-6">
+          <Avatar className="w-24 h-24 mb-4">
+            <AvatarImage src={profile.avatar_url || ""} alt="User avatar" />
+            <AvatarFallback className="hover:bg-muted/50">
+              <span className="text-4xl font-bold">
+                {profile.username.charAt(0).toUpperCase()}
+              </span>
+            </AvatarFallback>
+          </Avatar>
+          <h1 className="text-2xl font-bold mb-2">@{profile.username}</h1>
+          <p className="text-gray-600 text-center mb-4">{profile.bio}</p>
         </div>
-      ))}
+      </div>
     </main>
   );
 };
